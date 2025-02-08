fa1d51b873bda22d5e1160d8beca477701f1b674
Bilal VM <Bilal VM@BILALVM>
2025-02-08 17:36:34 +0000

    Add project files.

---
+++ DeepSeekWebApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using DeepSeekWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeepSeekWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
